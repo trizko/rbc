8fd178ddedb33367a9e3c1358560d0ae58ac0072
Tony Rizko <tony@rizkocircle.com>
2018-06-08 00:05:39 -0700

    Add simple tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io::Read;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+use std::str;
+
+fn handle_client(mut stream: TcpStream) {
+    loop {
+        let mut read = [0; 1028];
+        match stream.read(&mut read) {
+            Ok(n) => {
+                if n == 0 {
+                    break;
+                }
+                println!("{:?}", str::from_utf8(&read[0..n]));
+                stream.write(&read[0..n]).unwrap();
+            }
+            Err(err) => {
+                panic!(err);
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8333").unwrap();
+    println!("Listening on port 8333...");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(_) => {
+                println!("Error");
+            }
+        }
+    }
 }
